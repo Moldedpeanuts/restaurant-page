973e27dc9a5e85c803856c93ed443cfcf328dec1
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-12 17:07:14 +0200

    Append homepage h1 to content div

--- src/home.js
+++ src/home.js
@@ -2,8 +2,14 @@ import { contentDiv } from "./index.js";
 
 export function loadHome() {
     const homeSection = document.createElement("section");
+    homeSection.setAttribute("class", "home");
     contentDiv.appendChild(homeSection);
+
+    const homeTitle = document.createElement("h1");
+    homeTitle.textContent = "Winkie's"
+    contentDiv.appendChild(homeTitle);
     
     const descriptionDiv = document.createElement("div");
     contentDiv.appendChild(descriptionDiv);
 }
+
