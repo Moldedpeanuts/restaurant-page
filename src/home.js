bdc53b52571cfacd3bcf9dfc01ef6caeb5501686
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-12 11:05:37 +0200

    Add description div

--- src/home.js
+++ src/home.js
@@ -3,4 +3,12 @@ import { contentDiv } from "./index.js";
 export function loadHome() {
     const homeSection = document.createElement("section");
     contentDiv.appendChild(homeSection);
+
+    const descriptionDiv = document.createElement("div");
+    descriptionDiv.style.border = "3px solid #FFB800";
+    descriptionDiv.style.padding = "40px 20px";
+    descriptionDiv.style.width = "200px";
+    descriptionDiv.style.height = "180px";
+    descriptionDiv.style.borderRadius = "20px";
+    contentDiv.appendChild(descriptionDiv);
 }
