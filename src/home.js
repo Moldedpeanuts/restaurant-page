879495c72eb879dd40b56735540130897ad4c21f
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-12 17:18:24 +0200

    Append first para to div at home

--- src/home.js
+++ src/home.js
@@ -13,5 +13,9 @@ export function loadHome() {
     const descriptionDiv = document.createElement("div");
     descriptionDiv.setAttribute("class", "description")
     contentDiv.appendChild(descriptionDiv);
+    
+    const homePara1 = document.createElement("p");
+    homePara1.textContent = "Step into Winkie's where the coffee is hot, the pie is sweet, and the atmosphere is drenched in classic diner nostalgia. Nestled in the heart of Los Angeles, Winkie’s is more than just a roadside stop—it’s a place where late-night conversations linger, and the ordinary meets the mysterious. Whether you're here for a quick bite or something more, you never know what you might discover at Winkie’s. So grab a booth, sip your coffee, and let the story unfold.";
+    descriptionDiv.appendChild(homePara1);
 }
 
