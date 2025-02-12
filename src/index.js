03680ed6f5982259be7125227aee2ded6144643c
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-12 11:22:08 +0200

    Import css file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export const contentDiv = document.querySelector("#content");
 import { loadHome } from "./home";
+import './styles.css';
 
 document.addEventListener("DOMContentLoaded", () => {
     loadHome();
