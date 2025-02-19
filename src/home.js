ad7062134db5e3417d3bb7602d5c94527cb34362
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-19 15:19:23 +0200

    Add div for hours section

--- src/home.js
+++ src/home.js
@@ -17,5 +17,11 @@ export function loadHome() {
     const homePara1 = document.createElement("p");
     homePara1.textContent = "Step into Winkie's where the coffee is hot, the pie is sweet, and the atmosphere is drenched in classic diner nostalgia. Nestled in the heart of Los Angeles, Winkie’s is more than just a roadside stop—it’s a place where late-night conversations linger, and the ordinary meets the mysterious. Whether you're here for a quick bite or something more, you never know what you might discover at Winkie’s. So grab a booth, sip your coffee, and let the story unfold.";
     descriptionDiv.appendChild(homePara1);
+
+    const hoursDiv = document.createElement("div");
+    hoursDiv.setAttribute("class", "hours");
+    contentDiv.appendChild(hoursDiv);
+
+    
 }
 
