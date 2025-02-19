2407affec17d1d30a5dcfe05d332a48f29392d8a
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-19 15:43:47 +0200

    Add home title class to h1

--- src/home.js
+++ src/home.js
@@ -5,12 +5,11 @@ export function loadHome() {
     contentDiv.innerHTML = 
         `
             <section class="home">
-                <h1>Winkie's</h1>
+                <h1 class="home-title">Winkie's</h1>
                 <div class="description">
                     <p>
                         "Step into Winkie's where the coffee is hot, the pie is sweet, and the atmosphere is drenched in classic diner nostalgia. Nestled in the heart of Los Angeles, Winkie’s is more than just a roadside stop—it’s a place where late-night conversations linger, and the ordinary meets the mysterious. Whether you're here for a quick bite or something more, you never know what you might discover at Winkie’s. So grab a booth, sip your coffee, and let the story unfold."
                     </p>
-                    <span>David</span>
                 </div>
                 <div class="hours">
 
